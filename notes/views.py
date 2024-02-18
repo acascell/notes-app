53ab1649559eac2b556d0d64743603c60f9bf484
acascell <infocascella@gmail.com>
2024-02-18 20:14:35 +0100

    feat: api view for user registration

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from notes.serializers import UserSerializer
+
 
 # Create your views here.
+
+class UserCreate(generics.CreateAPIView):
+    authentication_classes = ()
+    permission_classes = ()
+    serializer_class = UserSerializer
