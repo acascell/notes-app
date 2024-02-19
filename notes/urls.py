8ffcbb0bb575e73b609ef4b35955393af21f600a
acascell <infocascella@gmail.com>
2024-02-19 09:55:56 +0100

    feat: updated urls matching new api endpoint

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserCreate
+from .views import UserCreate, NoteList, NoteDetail
 
 urlpatterns = [
     path('register/', UserCreate.as_view(), name='register'),
+    path('notes/', NoteList.as_view(), name='note-list'),
+    path('notes/<int:pk>/', NoteDetail.as_view(), name='note-detail'),
 ]
