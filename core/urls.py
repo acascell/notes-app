32c44b3c2a3724e945df5cb93011cde7e017a34b
acascell <infocascella@gmail.com>
2024-02-19 15:07:43 +0100

    feat: defined basic views
    - included multiple views rendering html templates --> login/index/register/notes
    - updated notelist permission class accepting session auth and basic auth
    - updated urls

--- core/urls.py
+++ core/urls.py
@@ -17,11 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from notes import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    #index
+    path('', views.index, name='index'),
+    # api
     path("api/", include("notes.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
