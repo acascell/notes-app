2d4c33962a8d98826c8f6d9f73992545a1385a23
acascell <infocascella@gmail.com>
2024-02-19 09:55:12 +0100

    feat: defined note serializer

--- notes/serializers.py
+++ notes/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from notes.models import Note
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +12,10 @@ class UserSerializer(serializers.ModelSerializer):
         
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'content', 'created_at', 'updated_at', 'owner']
+        read_only_fields = ['created_at', 'updated_at', 'owner']
