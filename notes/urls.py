92c6e63c960c32597614e783ceb3f77d7e8542b6
acascell <infocascella@gmail.com>
2024-02-27 14:08:26 +0100

    feat: updated urls routing

--- notes/urls.py
+++ notes/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import NoteList, NoteDetail, login_page, register_page, UserCreate, notes_page
+from .views import NoteList, NoteDetail, login_page, register_page, UserCreate, notes_page, list_notes, logout_page
 
 urlpatterns = [
     # Frontend URL for the login page
     path('login/', login_page, name='login'),
+    path('logout/', logout_page, name='logout'),
     path('register/', register_page, name='register'),
-    path('create-note', notes_page, name='create-note'),
+    path('create-note/', notes_page, name='create-note'),
+    path('list-notes/', list_notes, name='list-notes'),
 
     path('user-registration/', UserCreate.as_view(), name='user-registration'),
     path('notes/', NoteList.as_view(), name='note-list'),
